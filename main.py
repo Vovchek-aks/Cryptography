365607ee81fdf486e2f8006c5f3119aa4dd6a455
Vovchek <Vlad.aks03@mail.ru>
2021-07-11 20:17:39 +0500

    работает

--- main.py
+++ main.py
@@ -1,16 +1,101 @@
-# This is a sample Python script.
+import random as rand
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+LETTERS_CAN_BE_USED = 'йцукенгшщзхъфывапролджэячсмитьбюёqwertyuiopasdfghjklzxcvbnm'
+LETTERS_CAN_BE_USED += LETTERS_CAN_BE_USED.upper() + ''',.1234567890-=`~!@#$%^&*()_+[]{};: '"\\|<>/?\n'''
+
+LETTERS_TO_CODE = LETTERS_CAN_BE_USED
+
+# print(LETTERS_TO_CODE)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def key_to_seed(key: str) -> int:
+    seed = 2
+
+    for i in key:
+        seed += ord(i)
+        seed *= ord(i)
+
+    return seed
 
 
-# Press the green button in the gutter to run the script.
+def text_to_code(key: str, text: str) -> str:
+    seed = key_to_seed(key)
+    rand.seed(seed)
+
+    code = ''
+    for i in text:
+        if i not in LETTERS_CAN_BE_USED:
+            continue
+
+        n = [(rand.randint(0, len(LETTERS_TO_CODE) - 1)) for _ in range(rand.randint(1, 10))]
+        n = [j + LETTERS_TO_CODE.index(i) for j in n]
+        n = [j % len(LETTERS_TO_CODE) for j in n]
+        n = [LETTERS_TO_CODE[j] for j in n]
+
+        code += ''.join(n)
+
+    return code
+
+
+def code_to_text(key: str, code: str) -> str:
+    seed = key_to_seed(key)
+    rand.seed(seed)
+
+    text = ''
+    while code:
+        n = rand.randint(1, 10)
+
+        letter = LETTERS_TO_CODE.index(code[0])
+        code = code[n:]
+
+        rand_letter = [(rand.randint(0, len(LETTERS_TO_CODE) - 1)) for _ in range(n)][0]
+
+        if rand_letter > letter:
+            letter += len(LETTERS_TO_CODE)
+
+        target_letter = LETTERS_TO_CODE[letter - rand_letter]
+
+        text += target_letter
+
+    return text
+
+
+def my_input(fun) -> tuple:
+    print('Введите ключ\n')
+    key = input()
+    print('Введите имя файла\n')
+    file_name = input()
+    text = open(file_name, encoding='UTF-8').read()
+
+    return file_name, fun(key, text)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Закодировать - 0\n'
+          'Разкодировать - 1\n')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    num = 2
+
+    try:
+        num = int(input())
+    except Exception as x:
+        input(f'ERROR: {x}\n')
+        exit(0)
+
+    if num == 0:
+        func = text_to_code
+    elif num == 1:
+        func = code_to_text
+    else:
+        input('ERROR\n')
+        exit(0)
+
+    ret = my_input(func)
+
+    f = open(ret[0][:-4] + '_' + ('de' if num else '') + 'code.txt', 'w', encoding='UTF-8')
+    f.write(ret[1])
+    f.close()
+
+
+
+
